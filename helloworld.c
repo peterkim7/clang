b2dd387c28239a008092cdba5e5d34d370eae0ad
peterkim7 <157963533+peterkim7@users.noreply.github.com>
2024-09-13 09:57:25 +0900

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,15 @@
-// This is sample program
 #include <stdio.h>
-void main(void)
+int main()
 {
-  printf("hello world !\n");
+	short int i;
+	// scanf("%d", &i);
+
+	//printf("input is %d\n", i);
+	printf("Data size for int : %d \n", sizeof(int));
+	printf("Data size for short int : %d \n", sizeof(short int));
+	printf("Data size for char  : %d \n", sizeof(char));
+	printf("Data size for long : %d \n", sizeof(long));
+	printf("Data size for double : %d \n", sizeof(double));
+
+	return 0;
 }
